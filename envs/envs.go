60e8ccd0ce32da76c9e7668a052532672eebe5aa
r1nlaw <gru.zhizhi@mail.ru>
2024-11-04 12:59:15 +0300

    Создал .env развернул MongoDB, инициализировал внешние переменные окружения в сервис

---
+++ envs/envs.go
@@ -0,0 +1,35 @@
+package envs
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// Хранение данных значений ENV
+var ServerEnvs Envs
+
+// Структура для хранения значений ENV
+type Envs struct {
+	MONGO_INITDB_ROOT_PASSWORD string
+	MONGO_INITDB_ROOT_USERNAME string
+	MONGO_INITDB_PORT          string
+	MONGO_INITDB_HOST          string
+	NOTES_PORT                 string
+}
+
+// Инициализация значений ENV
+func LoadEnvs() error {
+	// Загрузка значений ENV.
+	// Если файл .env не найден, то выводим сообщение
+	if err := godotenv.Load(); err != nil {
+		return err
+	}
+	// Инициализация значений ENV
+	ServerEnvs.NOTES_PORT = os.Getenv("NOTES_PORT")
+	ServerEnvs.MONGO_INITDB_ROOT_USERNAME = os.Getenv("MONGO_INITDB_ROOT_USERNAME")
+	ServerEnvs.MONGO_INITDB_ROOT_PASSWORD = os.Getenv("MONGO_INITDB_ROOT_PASSWORD")
+	ServerEnvs.MONGO_INITDB_PORT = os.Getenv("MONGO_INITDB_PORT")
+	ServerEnvs.MONGO_INITDB_HOST = os.Getenv("MONGO_INITDB_HOST")
+	return nil
+}
--- server.go/server.go
+++ server.go/server.go
@@ -1,7 +1,19 @@
 package server
 
+import (
+	"log"
+	"todo_project/envs"
+)
+
 func InitServer() {
 	// Инициализация внешних значений ENV
+	errEnvs := envs.LoadEnvs()
+	if errEnvs != nil {
+		// Вывод сообщения об ошибке
+		log.Fatal("Ошибка инициализации ENV: ", errEnvs)
+	} else {
+		log.Println("Инициализация ENV прошла успешно")
+	}
 	// Инициализация базы данных
 }
 
