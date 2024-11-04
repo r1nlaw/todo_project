9383faaa15547928b8f649b8631791513b9b2076
r1nlaw <gru.zhizhi@mail.ru>
2024-11-04 14:23:11 +0300

    Создал обработку запроса для создания заметки

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
--- handlers/note_handlers.go
+++ handlers/note_handlers.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"todo_project/database"
+	"todo_project/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func GetNoteHandler(ctx *gin.Context) {
@@ -22,6 +26,35 @@ func UpdateNoteHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, "UpdateNoteHandler")
 }
 
+// Обработка запроса для создания заметки
 func CreateNoteHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "CreateNoteHandler")
+
+	// Создание новой заметки
+	var note models.Note
+	// Получаем данные из запроса
+	if err := ctx.ShouldBindJSON(&note); err != nil { // ctx.ShouldBindJSON - автоматически десериализует JSON и заполняет структуры Note
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "Неверные данные",
+		})
+		return
+	}
+
+	// Получить уникальный id
+	note.Id = uuid.New().String()
+	// Тестовый id Автора
+	note.AuthorID = 1
+
+	// Получаем коллекцию "notes"
+	collection := database.MongoClient.Database("admin").Collection(fmt.Sprintf("notes/%d", note.AuthorID))
+	// Вставляем заметку в коллекцию
+	_, errInsert := collection.InsertOne(ctx, note)
+	if errInsert != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": errInsert.Error()})
+	}
+	// Если ошибок нет, то возвращаем заметку и статус 200
+	ctx.JSON(http.StatusOK, gin.H{
+		"note":    note,
+		"message": "Заметка успешно создана",
+	})
+
 }
