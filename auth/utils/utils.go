3a647a9c0bdf37b3b9fe4c0e1da9a31d9bb8c020
r1nlaw <gru.zhizhi@mail.ru>
2024-11-11 19:26:35 +0300

    Внедрил генерацию JWT-токенов для пользователей

--- auth/utils/utils.go
+++ auth/utils/utils.go
@@ -1,6 +1,14 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"auth/envs"
+	"auth/models"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
 
 // Хэшируем пароль при помощи bcrypt
 func HashPassword(password string) (string, error) {
@@ -15,4 +23,46 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+// Функция для генерации JWT-токена
+func GenerateTokens(userID uint) (models.Tokens, error) {
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(), // Срок действия токена 24 часа (exp)
+	})
+
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 600).Unix(), // Срок действия токена 1 месяц
+	})
+	signedAccessToken, _ := accessToken.SignedString([]byte(envs.ServerEnvs.JWT_SECRET))
+
+	signedRefreshToken, _ := refreshToken.SignedString([]byte(envs.ServerEnvs.JWT_SECRET))
+
+	return models.Tokens{AccessToken: signedAccessToken, RefreshToken: signedRefreshToken}, nil
+}
+
+// Функция проверки JWT-токена
+
+func ValidateRefreshToken(tokenString string) (uint, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("неожиданный метод подписи: %v", token.Header["alg"])
+		}
+		return []byte(envs.ServerEnvs.JWT_SECRET), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		userIDValue, ok := claims["user_id"].(float64) // приведение к float64
+		if !ok {
+			return 0, fmt.Errorf("user_id claims is not float64")
+		}
+		return uint(userIDValue), nil // Конвертация float64 в uint
+	} else {
+		return 0, fmt.Errorf("недействительный токен")
+	}
 }
